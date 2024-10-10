c4bf6e6d5fbe126f62b07576b33dbc2f9a0e6fd2
Natalia Labunskaia <nlabunskya@gmail.com>
2024-10-10 11:30:47 +0400

    Step 6. Game Calc. Made a game logic in file calc.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 import readlineSync from 'readline-sync';
-import { getRandomInt, getRandomOperator } from './random.js';
 
-const greetUser = () => {
+const startGame = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
+  return userName;
 };
 
-const gameSteps = () => {
-  const randomInt1 = getRandomInt(1, 100);
-  const randomInt2 = getRandomInt(1, 100);
-  const randomOperator = getRandomOperator();
-  const randomExpression = `${randomInt1} ${randomOperator} ${randomInt2}`;
-  const answer = readlineSync.question(`Question: ${randomExpression} `);
-  console.log(`Your answer: ${answer}`);
-};
-
-export { greetUser, gameSteps };
+export default startGame;
--- src/random.js
+++ src/random.js
@@ -4,11 +4,4 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (tmpMax - tmpMin + 1) + tmpMin);
 };
 
-const getRandomOperator = () => {
-  const arr = ['+', '-', '*'];
-  const randomIndex = Math.floor(Math.random() * arr.length);
-  const randomOperator = arr[randomIndex];
-  return randomOperator;
-};
-
-export default { getRandomInt, getRandomOperator };
+export default getRandomInt;
