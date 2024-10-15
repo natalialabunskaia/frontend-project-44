07ac6881c18196b5ad90d361bea270b882248ce0
Natalia Labunskaia <nlabunskya@gmail.com>
2024-10-15 13:34:51 +0400

    Step 9. Game Prime. Made new game

---
+++ src/games/prime.js
@@ -0,0 +1,23 @@
+import startGame from '../index.js';
+import getRandomInt from '../random.js';
+
+const gameRules =
+  'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (num) => {
+  if (num <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
+const getQuestionAndAnswer = () => {
+  const question = getRandomInt(1, 100);
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+
+const gamePrime = () => startGame(gameRules, getQuestionAndAnswer);
+
+export default gamePrime;
